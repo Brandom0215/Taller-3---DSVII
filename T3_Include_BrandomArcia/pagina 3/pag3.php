c7c3b2666e7985afe4cf822829522034d7c9338c
AlexandraDeGracia <alexandra.degracia1@utp.ac.pa>
2026-04-16 17:48:59 -0500

    Add files via upload

---
+++ T3_Include_BrandomArcia/pagina 3/pag3.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <title>ODS - Acciones</title>
+
+  <link href="css/bootstrap.min.css" rel="stylesheet" />
+  <link href="css/estilos.css" rel="stylesheet" />
+</head>
+
+<body>
+
+<!-- HEADER -->
+<?php include("headerpag3.html"); ?>
+
+<div id="contenedor" class="container">
+
+  <!-- NAV -->
+  <?php include("navpag3.html"); ?>
+
+  <!-- BREADCRUMB -->
+  <?php include("breadcrumbpag3.html"); ?>
+
+  <!-- JUMBOTRON -->
+  <?php include("jumbotronpag3.html"); ?>
+
+  <!-- CARRUSEL -->
+  <?php include("carouselpag3.html"); ?>
+
+  <!-- GRID -->
+  <div class="row">
+
+    <!-- IZQUIERDA -->
+    <?php include("col_izqpag3.html"); ?>
+
+    <!-- CENTRO -->
+    <?php include("col_centropag3.html"); ?>
+
+    <!-- DERECHA -->
+    <?php include("col_derechapag3.html"); ?>
+
+  </div>
+
+  <!-- ACCORDION -->
+  <?php include("accordionpag3.html"); ?>
+
+  <!-- MODAL -->
+  <?php include("modalpag3.html"); ?>
+
+  <!-- PAGINACIÓN -->
+  <?php include("paginacionpag3.html"); ?>
+
+</div>
+
+<!-- FOOTER -->
+<?php include("footerpag3.html"); ?>
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
